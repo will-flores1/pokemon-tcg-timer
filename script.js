c44fc520863ad30f97adcfad5dca02a5efab4707
wilfredo-flores <willflores1221@gmail.com>
2023-08-22 17:26:14 -0400

    inital release

--- script.js
+++ script.js
@@ -5,55 +5,177 @@
 	const log = console.log.bind(console);
 	const timerAudio = new Audio(`./assets/time.mp3`);
 
-	/* Model eg. state, config, logic, etc. */
+	/* Custom Event */
+	// const startE = new Event("start", { bubbles: false });
+	// const stopE = new Event("stop", { bubbles: false });
+	// const switchE = new Event("switch", { bubbles: false });
+	// const resetE = new Event("reset", { bubbles: false });
+	// /* Event Dispatchers func */
+	// function _dispatchStartEvent() {
+	// 	document.dispatchEvent(startE);
+	// }
+	// function _dispatchStopEvent() {
+	// 	document.dispatchEvent(stopE);
+	// }
+	// function _dispatchSwitchEvent() {
+	// 	document.dispatchEvent(switchE);
+	// }
+	// function _dispatchResetEvent() {
+	// 	document.dispatchEvent(resetE);
+	// }
+
+	/* Model eg. config, state, logic, api, etc. */
 	const mdl = (() => {
-		const env = "production"; // "development" || "production
 		/* State */
 		const config = {
-			DEFAULT_DURATION: 90, // value is in seconds
-			AUTOSWITCH: true, // auto next players turn? T/F
-		};
-		const defaultState = {
-			PLAYING: false,
-			PAUSED: true,
-			timeLeft: config.DEFAULT_DURATION,
+			env: "development", // "development" || "production
+			DURATION: 90, // value is in seconds
+			AUTOSWITCH_ON: false, // auto next players turn? T/F
 		};
 		const state = {
-			PLAYING: false,
-			PAUSED: true,
-			timeLeft: config.DEFAULT_DURATION,
-			resetStartBtn: true,
+			timeLeft: config.DURATION,
 			timerInterval: null,
 		};
-		const stats = {
-			turns: 0,
-			totalTime: 0,
-		};
+		state["timeLeftFmt"] = _fmtTimeLeft();
+
+		// const stats = {
+		// 	turns: 0,
+		// 	totalTime: 0,
+		// };
+
+		/* util helpers */
+		function _fmtTimeLeft() {
+			let minutes = Math.floor(state.timeLeft / 60)
+				.toString()
+				.padStart(2, "0");
+			let seconds = (state.timeLeft % 60).toString().padStart(2, "0");
+			return `${minutes}:${seconds}`;
+		}
+		function _decrementTime() {
+			// handles decrement timeLeft & timeLeftFmt
+			// if (state.timeLeft <= -1) return;
+			--state.timeLeft;
+			state.timeLeftFmt = _fmtTimeLeft();
+		}
+		function sleep(ms) {
+			return new Promise((resolve) => setTimeout(resolve, ms));
+		}
+		function _resetTime() {
+			state.timeLeft = Number(config.DURATION);
+			state.timeLeftFmt = _fmtTimeLeft();
+		}
+		/* Loggers  */
+		// function _prntInfo(msg) {
+		// 	log({ message: msg, ...mdl.state });
+		// }
+		// function _printState() {
+		// 	let minutes = Math.floor(mdl.state.timeLeft / 60)
+		// 		.toString()
+		// 		.padStart(2, "0");
+		// 	let seconds = (mdl.state.timeLeft % 60).toString().padStart(2, "0");
+		// 	log({ ...mdl.state, timeLeft: `${minutes}: ${seconds}` });
+		// }
+		// function _printTimeLeft() {
+		// 	log(_fmtTimeLeft());
+		// }
+
+		/* Interval helper */
+		function _startInterval() {
+			if (state.timerInterval) return;
+
+			state.timerInterval = setInterval(async () => {
+				_decrementTime();
+				if (state.timeLeft <= -1) {
+					if (config.AUTOSWITCH_ON) {
+						nextTurn();
+					} else {
+						_stopInterval();
+						_resetTime();
+					}
+				}
+				log(state);
+			}, 1000);
+		}
+		function _stopInterval() {
+			if (!state.timerInterval) return;
+			clearInterval(state.timerInterval);
+			state.timerInterval = null;
+		}
+
+		/* Countdown helpers */
+		function _startCountdown() {
+			if (state.timeLeft <= -1) return;
+			if (state.timerInterval) return;
+
+			_startInterval();
+		}
+		function _stopCountdown() {
+			if (state.timeLeft <= -1) return;
+			if (!state.timerInterval) return;
+
+			_stopInterval();
+		}
+
+		/* API */
+		function play() {
+			_startCountdown();
+		}
+		function pause() {
+			_stopCountdown();
+		}
+		function nextTurn() {
+			_stopInterval();
+			_resetTime();
+			_startInterval();
+		}
+
+		function setDuration(dur) {
+			_stopInterval();
+			config.DURATION = dur;
+			_resetTime();
+		}
+		function setAutoSwitch(bool) {
+			_stopInterval();
+			config.AUTOSWITCH_ON = bool;
+			_resetTime();
+		}
 
 		return {
-			config,
-			state,
-			env,
+			play,
+			pause,
+			nextTurn,
+			getDuration: () => config.DURATION,
+			setDuration,
+			getAutoSwitch: () => config.AUTOSWITCH_ON,
+			setAutoSwitch,
+			getTimeFmt: () => state.timeLeftFmt,
+			getEnv: () => config.env,
 		};
 	})();
 
 	/* View eg. DOM, UI, etc. */
 	const view = (() => {
 		// DOM Elements
-		const _timerDisp = $("#timer");
-		const _toggleBtnDiv = $("#toggleBtnDiv");
-		const strtBtn = createButton("Start");
+		const timerDisplay = $("#timer");
+		const startBtn = createButton("Start");
 		const resumeBtn = createButton("Resume");
-		const stpBtn = createButton("Pause");
+		const pauseBtn = createButton("Pause");
+		const currBtn = $("#currBtn");
+		const currInstructions = $("#currInstructions");
 		const switchBtn = $("#switchBtn");
 		const durSelect = $("#durationSelect");
-		const automatedSwitch = $("#automatedSwitch");
+		const autoSwitch = $("#automatedSwitch");
 
-		const _keyStateDisp = $("#keyStateDisp");
+		/* Default values */
+		durSelect.value = mdl.getDuration();
+		timerDisplay.textContent = mdl.getTimeFmt();
+		currBtn.appendChild(startBtn);
+		autoSwitch.checked = mdl.getAutoSwitch();
 
 		/* Development Mode */
-		(function devMode() {
-			if (mdl.env === "development") {
+		mdl.getEnv() === "development" &&
+			(function devMode() {
+				// Add some dev elements
 				// create 4s dur option
 				const option = document.createElement("option");
 				option.setAttribute("value", "4");
@@ -66,11 +188,9 @@
 				devTitle.style.textAlign = "center";
 				devTitle.style.paddingTop = "10px";
 				document.body.prepend(devTitle);
-			} else if (mdl.env === "production") return;
-			else throw new Error("Invalid environment detected.");
-		})();
+			})();
 
-		// General Elements
+		// Create Elements
 		function createButton(txt) {
 			const button = document.createElement("button");
 			button.id = txt.toLowerCase() + "Btn";
@@ -78,268 +198,156 @@
 			return button;
 		}
 
-		function updateTimerDisplay() {
-			const minutes = Math.floor(mdl.state.timeLeft / 60)
-				.toString()
-				.padStart(2, "0");
-			const seconds = (mdl.state.timeLeft % 60).toString().padStart(2, "0");
-			_timerDisp.textContent = `${minutes}:${seconds}`;
-			document.title = `Pokémon Turn Timer - ${minutes}:${seconds}`;
-		}
-		function updateToggleButton() {
-			_toggleBtnDiv.textContent = "";
-			if (mdl.state.resetStartBtn) {
-				_toggleBtnDiv.appendChild(strtBtn);
-			} else if (mdl.state.PLAYING) {
-				_toggleBtnDiv.appendChild(stpBtn);
-			} else {
-				_toggleBtnDiv.appendChild(resumeBtn);
-			}
-		}
-		function updateKeyDisplay(eventType) {
-			_keyStateDisp.textContent = eventType;
-		}
-		updateTimerDisplay();
-		updateToggleButton();
-
 		return {
-			updateTimerDisplay,
-			updateToggleButton,
-			strtBtn,
+			timerDisplay,
+			startBtn,
 			resumeBtn,
-			stpBtn,
+			pauseBtn,
+			currBtn,
+			currInstructions,
 			switchBtn,
 			durSelect,
-			automatedSwitch,
-			updateKeyDisplay,
+			autoSwitch,
 		};
 	})();
 
 	/* Controller eg. event listeners, event dispatchers, etc. */
-	const ctlr = (function () {
-		// Logging
-		function _prntInfo(msg) {
-			log({ message: msg, ...mdl.state });
-		}
-		function _printState() {
-			let minutes = Math.floor(mdl.state.timeLeft / 60)
-				.toString()
-				.padStart(2, "0");
-			let seconds = (mdl.state.timeLeft % 60).toString().padStart(2, "0");
-			log({ ...mdl.state, timeLeft: `${minutes}: ${seconds}` });
-		}
-		function _printTimeLeft() {
-			let minutes = Math.floor(mdl.state.timeLeft / 60)
-				.toString()
-				.padStart(2, "0");
-			let seconds = (mdl.state.timeLeft % 60).toString().padStart(2, "0");
-			log(`${minutes}:${seconds}`);
-		}
+	const ctlr = (() => {
+		let intervalHandler = null;
+		function _startInterval() {
+			if (intervalHandler) return;
 
-		/* Logic helpers */
-		function _startCountdown() {
-			if (mdl.state.timeLeft <= 0) return;
-
-			_printTimeLeft();
-			view.updateTimerDisplay();
-			// start countdown
-			mdl.state.timerInterval = setInterval(function () {
-				mdl.state.timeLeft--;
-				_printTimeLeft();
-				view.updateTimerDisplay();
-
-				// If clock hits 0, stop it
-				if (mdl.state.timeLeft === 0) {
-					if (!mdl.config.AUTOSWITCH) _stopCountdown();
-					if (mdl.config.AUTOSWITCH) _autoswitch();
+			intervalHandler = setInterval(() => {
+				updateTimerDisplay();
+				log("front interval");
+				if (mdl.getTimeFmt() === "00:00") {
+					log(mdl.getTimeFmt());
+					if (mdl.getAutoSwitch()) {
+						_stopInterval();
+						_startInterval();
+					} else {
+						_stopInterval();
+						updateCurrButton("reset");
+						updateInstructions("reset");
+					}
 				}
 			}, 1000);
 		}
-		function _stopCountdown() {
-			clearInterval(mdl.state.timerInterval);
-			_printTimeLeft();
-			view.updateTimerDisplay();
-		}
-		function _resetCountdown() {
-			clearInterval(mdl.state.timerInterval);
-			mdl.state.timeLeft = mdl.config.DEFAULT_DURATION;
+		function _stopInterval() {
+			if (!intervalHandler) return;
+			clearInterval(intervalHandler);
+			intervalHandler = null;
 		}
 
-		function _autoswitch() {
-			_resetCountdown();
-			// 1 seconds sleep
-			new Promise((resolve, reject) => {
-				mdl.state.timerInterval = setTimeout(() => {
-					log("Next turn");
-					_startCountdown();
-					resolve();
-				}, 1000);
-			});
-		}
-		function _manualswitch() {
-			if (mdl.state.resetStartBtn) return;
-			_resetCountdown();
-			log("Next turn");
-			_strtState();
-			_startCountdown();
-		}
-		function _strtState() {
-			mdl.state.PLAYING = true;
-			mdl.state.PAUSED = false;
-		}
-		function _stpState() {
-			mdl.state.PLAYING = false;
-			mdl.state.PAUSED = true;
-		}
-
-		/* Logic functions */
-		function _togglePlayState() {
-			// flip state depending on current state playing/paused
-			if (mdl.state.PLAYING && !mdl.state.PAUSED) {
-				_stpState();
-				// _prntInfo("Timer paused!");
-				return;
-			} else if (!mdl.state.PLAYING && mdl.state.PAUSED) {
-				_strtState();
-				// _prntInfo("Timer started!");
-				return;
-			} else {
-				console.error(
-					"Invalid state detected. Resetting to a default state."
-				);
-				mdl.state.PLAYING = false; // set default state
-				mdl.state.PAUSED = true; // set default state
-				return;
+		/* Refresh display helpers */
+		function updateCurrButton(state) {
+			view.currBtn.textContent = "";
+			switch (state) {
+				case "play":
+					view.currBtn.appendChild(view.pauseBtn);
+					break;
+				case "pause":
+					view.currBtn.appendChild(view.resumeBtn);
+					break;
+				case "start":
+					view.currBtn.appendChild(view.pauseBtn);
+					break;
+				case "reset":
+					view.currBtn.appendChild(view.startBtn);
+					break;
+				default:
+					break;
 			}
 		}
-		function _toggleCountdown() {
-			// start/stop countdown according to state
-			if (mdl.state.PLAYING && !mdl.state.paused) {
-				_startCountdown();
-			} else if (!mdl.state.PLAYING && mdl.state.PAUSED) {
-				_stopCountdown();
-			} else {
-				console.error(
-					"Invalid state detected. Resetting to a default state."
-				);
-				mdl.state.timerInterval = null;
-				return;
+		function updateInstructions(state) {
+			view.currInstructions.textContent = "";
+			switch (state) {
+				case "play":
+					view.currInstructions.textContent = "pause";
+					break;
+				case "pause":
+					view.currInstructions.textContent = "resume";
+					break;
+				case "start":
+					view.currInstructions.textContent = "pause";
+					break;
+				case "reset":
+					view.currInstructions.textContent = "start";
+					break;
+				default:
+					break;
 			}
 		}
-
-		/* Event Dispatchers func */
-		function _dispatchStartEvent() {
-			document.dispatchEvent(startEvent);
+		function updateTimerDisplay() {
+			view.timerDisplay.textContent = mdl.getTimeFmt();
 		}
-		function _dispatchStopEvent() {
-			document.dispatchEvent(stopEvent);
-		}
-		function _dispatchSwitchEvent() {
-			document.dispatchEvent(switchEvent);
-		}
-		function _dispatchResetEvent() {
-			document.dispatchEvent(resetEvent);
-		}
-
-		/* Controller Service Functions */
-		function _strt() {
-			if (mdl.state.PLAYING) return;
-			mdl.state.resetStartBtn = false;
-
-			log("start");
-
-			_togglePlayState();
-			_toggleCountdown();
-		}
-		function _stp() {
-			if (mdl.state.PAUSED) return;
-			log("stop");
-
-			_togglePlayState();
-			_toggleCountdown();
-		}
-		function _nxtTurn() {
-			_manualswitch();
-		}
-
-		/* Controllers API */
-		function strt() {
-			_strt();
-			view.updateToggleButton();
-			_dispatchStartEvent();
-		}
-		function play() {
-			_strt();
-			view.updateToggleButton();
-			_dispatchStartEvent();
-		}
-		function pause() {
-			_stp();
-			view.updateToggleButton();
-			_dispatchStopEvent();
-		}
-		function switchPlayer() {
-			_nxtTurn();
-			view.updateToggleButton();
-			if (!mdl.state.resetStartBtn) _dispatchSwitchEvent();
-		}
-
-		/* Event Dispatchers */
-		const startEvent = new Event("start", { bubbles: false });
-		const stopEvent = new Event("stop", { bubbles: false });
-		const switchEvent = new Event("switch", { bubbles: false });
-		const resetEvent = new Event("reset", { bubbles: false });
 
 		/* Event listeners */
-		// buttons
-		view.strtBtn.addEventListener("click", strt);
-		view.resumeBtn.addEventListener("click", play);
-		view.stpBtn.addEventListener("click", pause);
-		view.switchBtn.addEventListener("click", switchPlayer);
-		// events
+		// buttons events
+		view.startBtn.addEventListener("click", function start() {
+			mdl.play();
+			_startInterval();
+			updateCurrButton("start");
+			updateInstructions("start");
+			updateTimerDisplay();
+		});
+		view.resumeBtn.addEventListener("click", function resume() {
+			mdl.play();
+			updateCurrButton("play");
+			updateInstructions("play");
+			updateTimerDisplay();
+			intervalHandler = setInterval(() => {
+				updateTimerDisplay();
+			}, 1000);
+		});
+		view.pauseBtn.addEventListener("click", function pause() {
+			if (mdl.getTimeFmt() === "-1:-1") return;
+			mdl.pause();
+			_stopInterval();
+			updateCurrButton("pause");
+			updateInstructions("pause");
+			updateTimerDisplay();
+		});
+		view.switchBtn.addEventListener("click", function _switch() {
+			mdl.nextTurn();
+			_stopInterval();
+			_startInterval();
+			updateCurrButton("play");
+			updateInstructions("play");
+			updateTimerDisplay();
+		});
+		// change event
 		view.durSelect.addEventListener("change", (ev) => {
-			mdl.config.DEFAULT_DURATION = Number(ev.target.value);
-			_dispatchResetEvent();
+			mdl.setDuration(ev.target.value); // resets server timer
+			_stopInterval();
+			updateTimerDisplay();
+			updateCurrButton("reset");
+			updateInstructions("reset");
 		});
-		view.automatedSwitch.addEventListener("change", (ev) => {
-			mdl.config.AUTOSWITCH = ev.target.checked;
-			log(mdl.config.AUTOSWITCH);
+		view.autoSwitch.addEventListener("change", (ev) => {
+			mdl.setAutoSwitch(ev.target.checked);
+			_stopInterval();
+			updateTimerDisplay();
+			updateCurrButton("reset");
+			updateInstructions("reset");
 		});
+		// keyboard events
 		document.addEventListener("keydown", (ev) => {
 			switch (ev.key) {
 				case " ":
-					switchPlayer();
+					// Next player
 					break;
 				case "b":
-					if (mdl.state.PLAYING) {
-						pause();
-					} else {
-						play();
-					}
+					// Start/Resume/Pause
 					break;
 				default:
 					break;
 			}
 		});
 		// custom events
-		document.addEventListener("start", (ev) => {
-			view.updateKeyDisplay("stop");
-		});
-		document.addEventListener("stop", (ev) => {
-			view.updateKeyDisplay("resume");
-		});
-		document.addEventListener("switch", (ev) => {
-			if (mdl.state.resetStartBtn) view.updateKeyDisplay("start");
-			view.updateKeyDisplay("stop");
-		});
-		document.addEventListener("reset", (ev) => {
-			view.updateKeyDisplay("start");
-			mdl.state.resetStartBtn = true;
-			mdl.state.timeLeft = mdl.config.DEFAULT_DURATION;
-			_resetCountdown();
-			_stpState();
-			view.updateToggleButton();
-			view.updateTimerDisplay();
-		});
+		document.addEventListener("start", (ev) => {});
+		document.addEventListener("stop", (ev) => {});
+		document.addEventListener("switch", (ev) => {});
+		document.addEventListener("reset", (ev) => {});
 	})();
 })();
